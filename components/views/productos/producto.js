f2f9a1058646415837dcdf99e9471dc28878089d
Sambrana Ivan <sambranaivan@gmail.com>
2019-04-23 22:53:51 -0300

    fetch y render

--- components/views/productos/producto.js
+++ components/views/productos/producto.js
@@ -19,60 +19,28 @@ export default class Productos extends React.Component {
         super(props);
 
         this.state = {
-        loading: false,
-        produtos: [],      
+        productos: false,      
         error: null,
         }
      
-        this.arrayholder = [] ;
+    
       }
 
-      _keyExtractor = (item, index) => item.id;
     
-    async componentDidMount() {
+    componentDidMount() {
+    
         this.getProductos();
-        //const productos = await api.fetchProductos();
-        //this.setState({ productos });
-        //const productos = await fetch(URL + 'producto_supermercado');
-        // const requestProductos = new Request(URL + 'producto_supermercado', 
-        // {method: 'GET'});
-
-        // fetch(requestProductos)
-        //   .then(response => {
-        //     if (response.status === 200) {
-        //       respuesta = JSON.parse(response._bodyText);
-        //       this.setState({data: respuesta});
-        //       //console.log(respuesta);
-        //       console.log('prod');
-        //       console.log(this.state.data);
-        //     }
-        //     else {
-        //       console.log("Error en productos");
-        //       console.log(respuesta.status);
-        //     }
-        //   })
-        //this.setState({ data: JSON.stringify(requestProductos), loading: false});
-
-        // try {
-        //   //Assign the promise unresolved first then get the data using the json method. 
-        //   const response = await fetch(URL + 'producto_supermercado');
-        //   const json = await response.json();
-        //   //const prod = await JSON.parse(productos);
-        //   this.setState({data: json.results});
-        //   console.log(json);
-        //   console.log(this.state.data);
-        // } catch(err) {
-        //           console.log("Error fetching data-----------", err);
-        // }
-        //this.arrayholder = this.state.data;
+      
     }
 
     componentWillMount() {
 
-      
+        console.log("View Productos")
+    
     }
 
-    getProductos = () => {
+  getProductos = async () =>{
+      console.log("Get Productos")
       const requestProductos = new Request("https://precios.mcypcorrientes.gob.ar/api/producto",
                 { method: 'GET'});
     
@@ -82,9 +50,9 @@ export default class Productos extends React.Component {
                     if (response.status === 200) {
     
                         productos = JSON.parse(response._bodyText);
-                        this.setState({ productos: productos });
-                        console.log(productos)
-                        this.render();
+                        this.setState({ productos: productos.data });
+                        
+
                     }
                     else {
                         console.log("ERROR EN NOTIFICACIONES")
@@ -96,78 +64,51 @@ export default class Productos extends React.Component {
 
     viewProductoListado = () =>{
       let botones = []
-
-      for (let index = 0; index < this.state.produtos.length; index++) {
+      let prod = this.state.productos
+      // console.log(this.state.productos)
+      for (let index = 0; index < prod.length; index++) {
           botones.push(
-              <TouchableOpacity
-                  // key={"cadena_" + index}
-                  // onPress={() => this.setProducto(this.state.produtos[index])}
-                  // style={styles.button}
-              >
-                  <Text styles={{ fontSize: 24, color: "green" }}> {this.state.produtos[index].id}</Text>
-              </TouchableOpacity>
+            <TouchableOpacity>
+
+              <Text key={"producto_" + index} styles={{ fontSize: 24, color: "green" }}> {prod[index].nombre}</Text>
+            </TouchableOpacity>
+             
           )
       }
       return <View style={styles.view}>
           <Text>Seleccionar Producto</Text>
-          <ScrollView>
+         
               {botones}
-          </ScrollView>
+          
       </View>
 }
 
-    SearchFilterFunction = text => {    
-        const newData = this.arrayholder.filter(item => {      
-          const itemData = `${item.marca_producto_id}`;
-           const textData = text.toUpperCase();
-           return itemData.indexOf(textData) > -1;    
-        });    
-        this.setState({ data: newData });  
-    };
+    // SearchFilterFunction = text => {    
+    //     const newData = this.arrayholder.filter(item => {      
+    //       const itemData = `${item.marca_producto_id}`;
+    //        const textData = text.toUpperCase();
+    //        return itemData.indexOf(textData) > -1;    
+    //     });    
+    //     this.setState({ data: newData });  
+    // };
 
-    _renderListItem(item){
-      console.log(item)
-        return(
-          <View>
-            <View style={{flexDirection:'row',width:'100%',backgroundColor:'red'}}>
-            <Text>{item.id}</Text>
-            <Text>{item.nombre}</Text>
-            </View>
-          </View>
-          )
-      }
+    // _renderListItem(item){
+    //   console.log(item)
+    //     return(
+    //       <View>
+    //         <View style={{flexDirection:'row',width:'100%',backgroundColor:'red'}}>
+    //         <Text>{item.id}</Text>
+    //         <Text>{item.nombre}</Text>
+    //         </View>
+    //       </View>
+    //       )
+    //   }
 
   render() {
     return (
+     
       <View style={styles.container}>
-          {/* <View style={{width: WIDTH, margin: 5}}>
-            <SearchBar
-                clearIcon={{ color: 'gray', size: 30 }}
-                searchIcon={{
-                size: 60,
-                color: 'black',
-                }}
-                inputStyle={{
-                backgroundColor: 'white',
-                color: 'black',
-                }}
-                onChangeText={(text) => this.SearchFilterFunction(text)}
-                containerStyle={{
-                backgroundColor: 'white',
-                borderWidth: 0,
-                borderTopWidth: 0,
-                borderBottomWidth: 0, 
-                borderRadius: 30,
-                borderColor: '#8CA853'
-                }}
-                placeholder='Buscar...' />
-          </View> */}
-          {/* <FlatList
-          data={this.state.productos}
-          renderItem={({item}) => <Text style={styles.item}>{item.id}</Text>}
-          keyExtractor={(x, i) => i} 
-        /> */}
-        {this.viewProductoListado()}
+        {this.state.productos && this.viewProductoListado() }
       </View>
     );
   }
